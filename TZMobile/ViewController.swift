6a129119de3363dced6e2d87526c5066954d9092
RomStark <rgtarkovskiy@edu.hse.ru>
2024-12-17 23:28:39 +0300

    init project

---
+++ TZMobile/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TZMobile
+//
+//  Created by Al Stark on 17.12.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
